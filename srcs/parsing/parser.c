3dae4d2633db7b0d391e7ebda11db5747c196c51
Priam <priam@DESKTOP-JTM9F9N.localdomain>
2026-05-28 13:48:46 +0200

    update

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: pserre-s <priaserre@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 12:55:10 by pserre-s          #+#    #+#             */
-/*   Updated: 2026/05/27 16:45:56 by pserre-s         ###   ########.fr       */
+/*   Updated: 2026/05/28 12:36:58 by pserre-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,39 @@ int	fill_map_data(int fd, t_map *map)
 {
 	char	*line;
 	char	*trimmed_line;
+	int     status;
 
 	line = get_next_line(fd);
 	while (line)
 	{
+		status = 0;
 		trimmed_line = ft_strtrim(line, " \t\n");
 		if (trimmed_line && trimmed_line[0])
 		{
 			if (!ft_strncmp(trimmed_line, "NO ", 3))
-                check_and_fill(map, NO, trimmed_line + 3);
+                status = check_and_fill(map, NO, trimmed_line + 3);
             else if (!ft_strncmp(trimmed_line, "SO ", 3))
-                check_and_fill(map, SO, trimmed_line + 3);
+                status = check_and_fill(map, SO, trimmed_line + 3);
             else if (!ft_strncmp(trimmed_line, "WE ", 3))
-                check_and_fill(map, WE, trimmed_line + 3);
+                status = check_and_fill(map, WE, trimmed_line + 3);
             else if (!ft_strncmp(trimmed_line, "EA ", 3))
-                check_and_fill(map, EA, trimmed_line + 3);
+                status = check_and_fill(map, EA, trimmed_line + 3);
             else if (!ft_strncmp(trimmed_line, "F ", 2))
-                check_and_fill(map, F, trimmed_line + 2);
+                status = check_and_fill(map, F, trimmed_line + 2);
             else if (!ft_strncmp(trimmed_line, "C ", 2))
-                check_and_fill(map, C, trimmed_line + 2);
+                status = check_and_fill(map, C, trimmed_line + 2);
+			if (status == 1)
+			{
+				free(line);
+				free(trimmed_line);
+				line = get_next_line(fd);
+				while (line)
+				{
+					free(line);
+					line = get_next_line(fd);
+				}
+				return (0);
+			}
 		}
 		free(line);
 		free(trimmed_line);
@@ -65,7 +79,7 @@ int	parse_map(char *map, t_data *data)
 	if (!fill_map_data(fd, &data->map))
 	{
 		close(fd);
-		return (ft_putstr_fd("Error: fill map failed", 2), 0);
+		return (ft_putstr_fd("Error: fill map failed\n", 2), 0);
 	}
 	close(fd);
 	return (1);
