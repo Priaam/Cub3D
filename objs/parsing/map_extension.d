177fcdf40f4d79e053476b58c73671b8b4f9251a
pserre-s <pserre-s.42.fr>
2026-06-01 17:16:40 +0200

    update

--- objs/parsing/map_extension.d
+++
@@ -1,5 +0,0 @@
-objs/parsing/map_extension.o: srcs/parsing/map_extension.c \
- include/Cub3d.h libs/Libft/include/libft.h libs/minilibx-linux/mlx.h
-include/Cub3d.h:
-libs/Libft/include/libft.h:
-libs/minilibx-linux/mlx.h:
--- objs/parsing/parser.d
+++
@@ -1,5 +0,0 @@
-objs/parsing/parser.o: srcs/parsing/parser.c include/Cub3d.h \
- libs/Libft/include/libft.h libs/minilibx-linux/mlx.h
-include/Cub3d.h:
-libs/Libft/include/libft.h:
-libs/minilibx-linux/mlx.h:
