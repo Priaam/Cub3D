8f281c31958c3decb81f08f407f94275fdee5ef2
Priam <priam@DESKTOP-JTM9F9N.localdomain>
2026-06-07 16:14:56 +0200

    update

--- objs/main.d
+++
@@ -1,6 +0,0 @@
-objs/main.o: srcs/main.c include/Cub3d.h libs/Libft/include/libft.h \
- libs/minilibx-linux/mlx.h include/minimap.h
-include/Cub3d.h:
-libs/Libft/include/libft.h:
-libs/minilibx-linux/mlx.h:
-include/minimap.h:
